82a521ae88c4a97ad0fb21b42a7cd36d4f1f686a
Sasa Radovanovic <salle90ar@gmail.com>
2020-06-07 13:27:41 +0200

    content style

--- js/app.js
+++ js/app.js
@@ -1,9 +1,11 @@
 // Header toggle
 const header = document.querySelector('.header');
 const headerBurger = document.querySelector('.header-burger');
+const content = document.querySelector('.content');
 
 const toggleHeader = () => {
   header.classList.toggle('active');
+  content.classList.toggle('active');
 };
 
 headerBurger.addEventListener('click', toggleHeader);
