dac69738ed4d3a272865c023fcf525fd5506f638
Sasa Radovanovic <salle90ar@gmail.com>
2020-06-07 23:29:11 +0200

    home header

--- js/app.js
+++ js/app.js
@@ -9,3 +9,57 @@ const toggleHeader = () => {
 };
 
 headerBurger.addEventListener('click', toggleHeader);
+
+// Arrow
+const arrow = document.querySelector('.down-arrow');
+const arrowUp = document.querySelector('.down-arrow.up');
+
+let last_known_scroll_position = 0;
+let ticking = false;
+
+function downscroll(scroll_pos) {
+  arrow.classList.add('up');
+}
+
+function upscroll(scroll_pos) {
+  arrow.classList.remove('up');
+}
+
+window.addEventListener('scroll', function (e) {
+  let st = window.scrollY;
+
+  if (st > last_known_scroll_position) {
+    // Downscroll Code
+    if (!ticking) {
+      window.requestAnimationFrame(function () {
+        downscroll(last_known_scroll_position);
+        ticking = false;
+      });
+
+      ticking = true;
+    }
+  } else {
+    // Upscroll Code
+    if (!ticking) {
+      window.requestAnimationFrame(function () {
+        upscroll(last_known_scroll_position);
+        ticking = false;
+      });
+
+      ticking = true;
+    }
+  }
+
+  last_known_scroll_position = st;
+});
+
+// Sroll Up
+const scrollUp = () => {
+  window.scroll({
+    top: 0,
+    left: 0,
+    behavior: 'smooth',
+  });
+};
+
+arrow.addEventListener('click', scrollUp);
